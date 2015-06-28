a7121aa731042506b27dddb43b0349fbdcc644b2
Qimu Zheng <zheng.qm@163.com>
2015-06-28 20:27:26 +0800

    Initial Commit

---
+++ RGB Guess/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RGB Guess
+//
+//  Created by 郑淇木 on 6/28/15.
+//  Copyright (c) 2015 Qimu Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
